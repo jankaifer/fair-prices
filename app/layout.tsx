0f42471a026a348597aa7c5ef44e8e084532f448
Jan Kaifer <jan@kaifer.cz>
2024-01-25 21:42:24 +0100

    added mantine

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import "@mantine/core/styles.css";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import { Button } from "@mantine/core";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +21,15 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <head>
+          <ColorSchemeScript />
+        </head>
+        <body className={inter.className}>
+          <MantineProvider>
+            <Button>I'm button</Button>
+            {children}
+          </MantineProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
